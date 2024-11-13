b0674e9774f0f04c4948ac5348f4871a141d7780
MaraAnima <mara.duarte@anima.edu.uy>
2024-11-12 21:16:40 -0300

    Hover Movies
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: Natayha-Raicoff <179271466+Natayha-Raicoff@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+<<<<<<< HEAD
+=======
+//import { useState } from 'react'
+import './App.css'
+import MovieCard from './components/MovieCard'
+import Movies from './components/movies'
+import RatingStar from "./components/RatingStar";
+import MovieNavbar from './components/MovieNavbar'
+
+
+function App() {
+  const defaultRating = localStorage.getItem("starRating");
+
+  return (
+    
+    <> 
+    <MovieNavbar/>
+    <div>
+      <RatingStar iconSize={50} defaultRating={defaultRating} />
+    </div>
+
+    <div className="row">
+    <div className="box col-4" >
+    {Movies.map((movies) => (
+      <div className='movie'>
+       <MovieCard
+          key={movies.id}
+          name={movies.name}
+          poster={movies.poster}
+        />
+       </div>
+    ))}
+  </div>
+         </div>
+  </>
+     
+
+    
+  )
+}
+
+export default App
+>>>>>>> parent of f3cc74e (Rating Stars)
