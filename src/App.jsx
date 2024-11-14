4ec0a280bc8501c549f8e8c5f10794443e3ba2f5
MaraAnima <mara.duarte@anima.edu.uy>
2024-11-14 18:51:49 -0300

    Navbar pelado
    le falta todo estilo
    
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: Natayha-Raicoff <179271466+Natayha-Raicoff@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import RatingStar from "./components/RatingStar";
 import MovieBanner from "./components/MovieBanner";
+import MovieNavbar from "./components/MovieNavbar"
 
 function App() {
   return (
     <>
-      <div>
+      <MovieNavbar/>
         <MovieBanner />
-      </div>
       <div className="container">
         <RatingStar />
         <MovieList />
---
+++ src/components/MovieNavbar.jsx
@@ -0,0 +1,21 @@
+import "./MovieNavbar.css";
+
+function MovieNavbar() {
+    return (
+        <>
+        <div> 
+        <header className="header">
+            <img src="" alt="" className="" />
+            <nav>
+                <ul>
+                    <a href="/">Home</a>
+                </ul>
+            </nav>
+        </header>
+        </div>
+        </>
+
+    )
+
+}
+export default MovieNavbar;
