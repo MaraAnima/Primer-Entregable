8636f75d8fce7b9b6611bc8b178ebb7590dc3988
MaraAnima <mara.duarte@anima.edu.uy>
2024-11-14 03:12:20 -0300

    Banner Terminado
    Habria que poner cosas mas lindas de fondo  y eso
    
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: Natayha-Raicoff <179271466+Natayha-Raicoff@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import MovieList from "./components/MovieList";
 import RatingStar from "./components/RatingStar";
+import MovieBanner from "./components/MovieBanner";
 
 function App() {
   return (
     <>
+     
     <div className="container">
+    <MovieBanner/>
     <RatingStar />
         <MovieList />
+        
         </div>
+       
     </>
   );
 }
