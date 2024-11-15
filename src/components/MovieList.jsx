46c6b0f4f1625e4fd4a85d1d11dd4833710e10b1
Natayha-Raicoff <natayharaicoff4@gmail.com>
2024-11-14 21:41:51 -0300

    ReatingStar no terminado
    falta filtrar bien las estrellas con un fetch y no local
    
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: SaraOlivera222 <179405507+SaraOlivera222@users.noreply.github.com>
    Co-Authored-By: MaraAnima <179888074+MaraAnima@users.noreply.github.com>

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 
-
-function MovieList() {
-  const [movies, setMovies] = useState([]);
-
+function MovieList({ rating, movies, setMovies }) {
   async function fetchMovies() {
     const response = await fetch(
       "https://api.themoviedb.org/3/discover/movie?api_key=33351da04ac1d34c5939d977f66571b0&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40"
@@ -16,22 +13,32 @@ function MovieList() {
     fetchMovies();
   }, []);
 
+  const minRating = (rating - 1) * 2;
+  const maxRating = rating * 2;
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.vote_average >= minRating && movie.vote_average <= maxRating
+  );
+
   return (
-
-      <div className="row">
-    {movies.map((movie) => (
-      /*Puse el formato de como se muestran las peliculas en otro componente para que quede mas limpio, ademas facilita ponerle hovers*/
-      <div key={movie.id} className=" col-3 mt-4 mb-4">
-        <MovieCard
-          title={movie.title}
-          poster_path={movie.poster_path}
-          vote_average={movie.vote_average}
-        />
-      </div>
-
-    ))}
-  </div>
-  
+    <div className="row">
+      {filteredMovies.length > 0 ? (
+        filteredMovies.map((movie) => (
+          <div key={movie.id} className="col-3 mt-4 mb-4">
+            <MovieCard
+              title={movie.title}
+              poster_path={movie.poster_path}
+              vote_average={movie.vote_average}
+            />
+          </div>
+        ))
+      ) : (
+        <p>
+          Lo sentimos, no se encontraron películas con el rating solicitado.
+        </p>
+      )}
+    </div>
   );
 }
+
 export default MovieList;
