a0c57e6a4494c6c3e37aad9b9cbcd063b2adfd13
MaraAnima <mara.duarte@anima.edu.uy>
2024-11-15 22:46:28 -0300

    Funcionalidad terminada
    Solo falta diseño
    
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: Natayha-Raicoff <179271466+Natayha-Raicoff@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,33 +1,66 @@
 import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
 
 function MovieList({ rating }) {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);  
+  const [hasMore, setHasMore] = useState(true);  
 
+ 
+  const fetchMovies = async (reset = false) => {
+    if (reset) {
+      setMovies([]);  
+      setPage(2);
+    }
+    
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=33351da04ac1d34c5939d977f66571b0&include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&vote_average.gte=${rating}&vote_count.gte=80`
+    );
+    const data = response.data;
+    setMovies((prevMovies) => [...prevMovies, ...data.results]); 
+    setHasMore(data.results.length > 0); 
+  };
+
+ 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=33351da04ac1d34c5939d977f66571b0&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&vote_average.gte=${rating}&vote_count.gte=80 `
-      );
-      setMovies(response.data.results);
-    };
-    fetchMovies();
-  }, [rating]);
+    fetchMovies(true); 
+  }, [rating]); 
+
+  const fetchMoreMovies = () => {
+    if (hasMore) {
+      setPage((prevPage) => {
+        const nextPage = prevPage + 1;
+        fetchMovies();  
+        return nextPage;  
+      });
+    }
+  };
 
   return (
-    <div className="row">
-      {movies.map((movie) => (
-        /*Puse el formato de como se muestran las peliculas en otro componente para que quede mas limpio, ademas facilita ponerle hovers*/
-        <div key={movie.id} className=" col-3 mt-4 mb-4">
-          <MovieCard
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-          />
+    <div>
+      <InfiniteScroll
+        dataLength={movies.length} 
+        next={fetchMoreMovies}
+        hasMore={hasMore}  
+        loader={<h4>Loading...</h4>} 
+        endMessage={<p>No more movies!</p>} 
+      >
+        <div className="row">
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-3 mt-4 mb-4">
+              <MovieCard
+                title={movie.title}
+                poster_path={movie.poster_path}
+                vote_average={movie.vote_average}
+              />
+            </div>
+          ))}
         </div>
-      ))}
+      </InfiniteScroll>
     </div>
   );
 }
+
 export default MovieList;
