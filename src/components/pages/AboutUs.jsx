2053d08fdeb4a1caf4e729d504812b21d6eb02a5
MaraAnima <mara.duarte@anima.edu.uy>
2024-11-28 03:19:46 -0300

    Nosotros uwu 👉👈
    Co-Authored-By: LucasSanmartino7 <179268813+LucasSanmartino7@users.noreply.github.com>
    Co-Authored-By: Natayha-Raicoff <179271466+Natayha-Raicoff@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,21 +1,51 @@
 import MovieNavbar from "./MovieNavbar";
 import Foot from "./Foot";
+import Paloma from "../../assets/img/Paloma.jpg";
 
 function AboutUs() {
   return (
     <>
       <MovieNavbar />
-
-      <div className="defaultBox row">
-        <h1>About Us</h1>
-        <p>
-          Lore ipsum dolor sit amet consectetu adipisicing elit. Modi beatae id,
-          soluta iure expedita corrupti molestiae. Magni sequi sint beatae
-          ratione corporis illum est debitis error iusto, ea esse laboriosam!
-          Molestias quo totam commodi pariatur est aliquam nam, saepe quibusdam!
-        </p>
+      <div className="defaultBox ">
+        <div className="Info-container ">
+          <div className="">
+            <h1>Our team</h1>
+          </div>
+          <div className="card-position-center">
+            <div className="card">
+              <img src={Paloma} alt="" className="fotos" />
+              <h3>Sara</h3>
+            </div>
+          </div>
+          <div className="card-position ">
+            <div className="card">
+              <img src={Paloma} alt="" className="fotos" />
+              <h3>Sara</h3>
+            </div>
+            <div className="card">
+              <img src={Paloma} alt="" className="fotos" />
+              <h3>Mara</h3>
+            </div>
+            <div className="card">
+              <img src={Paloma} alt="" className="fotos" />
+              <h3>Nat</h3>
+            </div>
+            <div className="card">
+              <img src={Paloma} alt="" className="fotos" />
+              <h3>Lucas</h3>
+            </div>
+          </div>
+          <div className="defaultBox">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+              beatae id, soluta iure expedita corrupti molestiae. Magni sequi
+              sint beatae ratione corporis illum est debitis error iusto, ea
+              esse laboriosam! Molestias quo totam commodi pariatur est aliquam
+              nam, saepe quibusdam!
+            </p>
+          </div>
+        </div>
       </div>
-
       <Foot />
     </>
   );
